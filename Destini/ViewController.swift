c0e3323df65e934eabc091edf37586b971823847
t8lv4 <morgan@t8lv4.fr>
2018-05-30 17:46:12 +0200

    Refactor project
    create struct storyTexts
    create func goFromStoryX()
    	call goFromStoryX() from switch cases

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -11,39 +11,19 @@ import UIKit
 class ViewController: UIViewController {
 
     // Our strings
-    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
-    let answer1a = "I\'ll hop in. Thanks for the help!"
-    let answer1b = "Better ask him if he\'s a murderer first."
-    
-    
-    let story2 = "He nods slowly, unphased by the question."
-    let answer2a = "At least he\'s honest. I\'ll climb in."
-    let answer2b = "Wait, I know how to change a tire."
-    
-    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
-    let answer3a = "I love Elton John! Hand him the cassette tape."
-    let answer3b = "It\'s him or me! You take the knife and stab him."
-    
-    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
-    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
-    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
-    
-    
-    // UI Elements linked to the storyboard
+    let text = StoryTexts()
+    var storyIndex: Int = 1
+
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
     
-    var storyIndex: Int = 1
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyTextView.text = story1
-        topButton.setTitle(answer1a, for: .normal)
-        bottomButton.setTitle(answer1b, for: .normal)
+        storyTextView.text = text.story1
+        topButton.setTitle(text.answer1a, for: .normal)
+        bottomButton.setTitle(text.answer1b, for: .normal)
     }
 
     
@@ -51,61 +31,69 @@ class ViewController: UIViewController {
     
         switch storyIndex {
         case 1:
-            if sender.tag == 1 {
-                storyTextView.text = story3
-                storyIndex = 3
-                
-                sender.setTitle(answer3a, for: .normal)
-                bottomButton.setTitle(answer3b, for: .normal)
-            }
-            
-            if sender.tag == 2 {
-                storyTextView.text = story2
-                storyIndex = 2
-                
-                sender.setTitle(answer2b, for: .normal)
-                topButton.setTitle(answer2a, for: .normal)
-            }
+            goFromStory1(sender: sender)
         case 2:
-            if sender.tag == 1 {
-                storyTextView.text = story3
-                storyIndex = 3
-                
-                sender.setTitle(answer3a, for: .normal)
-                bottomButton.setTitle(answer3b, for: .normal)
-            }
-            
-            if sender.tag == 2 {
-                storyTextView.text = story4
-                storyIndex = 4
-                
-                sender.isHidden = true
-                topButton.isHidden = true
-            }
+            goFromStory2(sender: sender)
         case 3:
-            if sender.tag == 1 {
-                storyTextView.text = story6
-                storyIndex = 6
-                
-                sender.isHidden = true
-                bottomButton.isHidden = true
-            }
-            
-            if sender.tag == 2 {
-                storyTextView.text = story5
-                storyIndex = 5
-                
-                sender.isHidden = true
-                topButton.isHidden = true
-            }
+            goFromStory3(sender:sender)
         default:
             break
         }
-    
     }
     
-
-
-
+    private func goFromStory1(sender: UIButton) {
+        if sender.tag == 1 {
+            storyTextView.text = text.story3
+            storyIndex = 3
+            
+            sender.setTitle(text.answer3a, for: .normal)
+            bottomButton.setTitle(text.answer3b, for: .normal)
+        }
+        
+        if sender.tag == 2 {
+            storyTextView.text = text.story2
+            storyIndex = 2
+            
+            sender.setTitle(text.answer2b, for: .normal)
+            topButton.setTitle(text.answer2a, for: .normal)
+        }
+    }
+    
+    private func goFromStory2(sender: UIButton) {
+        if sender.tag == 1 {
+            storyTextView.text = text.story3
+            storyIndex = 3
+            
+            sender.setTitle(text.answer3a, for: .normal)
+            bottomButton.setTitle(text.answer3b, for: .normal)
+        }
+        
+        if sender.tag == 2 {
+            storyTextView.text = text.story4
+            storyIndex = 4
+            
+            sender.isHidden = true
+            topButton.isHidden = true
+        }
+    }
+    
+    private func goFromStory3(sender: UIButton) {
+        if sender.tag == 1 {
+            storyTextView.text = text.story6
+            storyIndex = 6
+            
+            sender.isHidden = true
+            bottomButton.isHidden = true
+        }
+        
+        if sender.tag == 2 {
+            storyTextView.text = text.story5
+            storyIndex = 5
+            
+            sender.isHidden = true
+            topButton.isHidden = true
+        }
+    }
+    
 }
 
